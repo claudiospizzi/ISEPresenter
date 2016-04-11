b1edfb70bddd90b239aae456db96742a38e3dc6b
Claudio Spizzi <claudio.spizzi@live.com>
2016-04-11 13:43:06 +0200

    Improve CI build

---
+++ Scripts/publish.ps1
@@ -0,0 +1,11 @@
+﻿
+# Definition for build
+$Module = 'ISEPresenter'
+$Source = "C:\Projects\$Module"
+
+# Extract module version
+$ModuleVersion = (Invoke-Expression -Command (Get-Content -Path "$Source\bin\Release\$Module.psd1" -Raw)).ModuleVersion
+
+# Push appveyor artifacts
+Compress-Archive -Path "$Source\bin\Release" -DestinationPath "$Source\$Module-$ModuleVersion-$env:APPVEYOR_BUILD_VERSION.zip"
+Push-AppveyorArtifact -Path "$Source\$Module-$ModuleVersion-$env:APPVEYOR_BUILD_VERSION.zip" -DeploymentName $Module
