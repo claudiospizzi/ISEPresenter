2416c862f16a0604aeef905d350ca89b85db9ca9
Claudio Spizzi <claudio.spizzi@live.com>
2016-04-11 13:59:07 +0200

    Fix artifacts zip file structure

--- Scripts/publish.ps1
+++ Scripts/publish.ps1
@@ -6,6 +6,9 @@ $Source = "C:\Projects\$Module"
 # Extract module version
 $ModuleVersion = (Invoke-Expression -Command (Get-Content -Path "$Source\bin\Release\$Module.psd1" -Raw)).ModuleVersion
 
+# Rename the release folder
+Rename-Item -Path "$Source\bin\Release" -NewName $Module
+
 # Push appveyor artifacts
-Compress-Archive -Path "$Source\bin\Release" -DestinationPath "$Source\$Module-$ModuleVersion-$env:APPVEYOR_BUILD_VERSION.zip"
+Compress-Archive -Path "$Source\bin\$Module" -DestinationPath "$Source\$Module-$ModuleVersion-$env:APPVEYOR_BUILD_VERSION.zip"
 Push-AppveyorArtifact -Path "$Source\$Module-$ModuleVersion-$env:APPVEYOR_BUILD_VERSION.zip" -DeploymentName $Module
